5b001968a4dafa0551ba77c57883d06bad30b250
himanoa <matsunoappy@gmail.com>
2019-01-19 07:41:17 +0900

    Fix style

--- components/Header.vue
+++ components/Header.vue
@@ -5,20 +5,21 @@
         to="/"
         class="no-underline text-white visited"><span class="font-bold text-xl tracking-wide hover:text-green-light">遺言書</span></nuxt-link>
     </div>
-    <div class="w-full block lg:flex lg:items-center lg:w-auto justify-end">
-      <div class="text-sm lg:flex-grow">
-        <a
-          href="https://himanoa.net"
-          class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-green-light mr-4 tracking-wide no-underline text-lg">
-          About
-        </a>
-        <a
-          href="https://twitter.com/h1manoa"
-          class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-green-light mr-4 tracking-wide no-underline text-lg">
-          Twitter
-        </a>
-      </div>
+    <div
+      class="block lg:hidden">
+      <button
+        class="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white"
+        @click="toggleExpand">
+        <svg
+          class="fill-current h-3 w-3"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+      </button>
     </div>
+    <HeaderItems class="w-full block lg:flex lg:items-center lg:w-auto justify-end sm:hidden" />
+    <HeaderItems
+      v-show="isExpand"
+      class="w-full block lg:flex lg:items-center lg:w-auto justify-end"/>
   </nav>
 </template>
 <style scoped>
@@ -28,7 +29,18 @@
 </style>
 <script lang="ts">
 import TwitterLogo from './TwitterLogo.vue'
+import HeaderItems from './HeaderItems.vue'
 export default {
-  components: { TwitterLogo }
+  components: { TwitterLogo, HeaderItems },
+  data() {
+    return {
+      isExpand: false
+    }
+  },
+  methods: {
+    toggleExpand() {
+      this.isExpand = !this.isExpand
+    }
+  }
 }
 </script>
---
+++ components/HeaderItems.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="text-sm lg:flex-grow">
+    <a
+      href="https://himanoa.net"
+      class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-green-light mr-4 tracking-wide no-underline text-lg">
+      About
+    </a>
+    <a
+      href="https://twitter.com/h1manoa"
+      class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-green-light mr-4 tracking-wide no-underline text-lg">
+      Twitter
+    </a>
+  </div>
+</template>
+<script>
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    hidden: {
+      type: Boolean,
+      default: false
+    }
+  }
+})
+</script>
