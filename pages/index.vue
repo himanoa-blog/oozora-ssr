082723a1c6c5b4eb0c037c7f838b57d2d6c3ec94
himanoa <matsunoappy@gmail.com>
2019-01-26 20:04:37 +0900

    Implements infinite scroll

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <section>
+      <Spinner />
       <EntryList
         :entries="entries"
       />
@@ -10,24 +11,54 @@
 
 <script lang="ts">
 import EntryList from '~/components/EntryList.vue'
+import Spinner from '~/components/Spinner.vue'
 import axios from '~/plugins/axios'
 import Vue from 'vue'
 
+const limit = 15
 export default Vue.extend({
   layout: 'default',
   components: {
-    EntryList
+    EntryList,
+    Spinner
   },
   data() {
     return {
       offset: 0,
       entries: [],
-      entriesCount: 0
+      entriesCount: 0,
+      loading: false
+    }
+  },
+  mounted() {
+    document.addEventListener('scroll', async e => {
+      const scrollingPosition =
+        document.documentElement.scrollTop + window.innerHeight
+      const bottomPosition = document.documentElement.offsetHeight
+
+      if (!this.loading && scrollingPosition >= bottomPosition) {
+        this.loading = true
+        await this.fetchData()
+        this.loading = false
+      }
+    })
+  },
+  beforeDestroy() {
+    console.dir('beforeDestroy')
+  },
+  methods: {
+    fetchData() {
+      return axios
+        .get(`/entries?offset=${this.offset}&limit=${limit}`)
+        .then(res => {
+          this.offset = this.offset + res.data.entries.length
+          this.entries = [...this.entries, ...res.data.entries]
+          this.entriesCount = res.data.count
+        })
     }
   },
   asyncData({ params }) {
     const offset = params.offset || 0
-    const limit = 15
     return axios.get(`/entries?offset=0&limit=${limit}`).then(res => {
       return {
         offset: offset + limit,
